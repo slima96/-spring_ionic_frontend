75ab54c6159a852590b399fb74b0619dc6b0cbd4
Alexsandro Leão Lima <sandro.lima@viatek.local>
2020-11-22 04:17:30 -0300

    Carregando dados do produto

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -11,12 +11,21 @@ export class ProdutoService {
         public http: HttpClient){
     }
 
+    findById(produto_id : string){
+        return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
+    }
+
     findAll() : Observable<ProdutoDTO[]> {
         return this.http.get<ProdutoDTO[]>(`${API_CONFIG.baseUrl}/produtos`);
     }
 
     getSmallImageFromBucket(id: string): Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
+        return this.http.get(url, {responseType: 'blob'});
+    }
+
+    getImageFromBucket(id: string): Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`
         return this.http.get(url, {responseType: 'blob'});
     }
 }
